0fbba91ee06b53cc477c5cb7c9f3e3387fcac66f
MrTan <1120610800@qq.com>
2016-08-10 22:46:33 +0800

    修改字段类型

--- app/src/main/java/com/example/mrtan/weather/data/model/Weather.java
+++ app/src/main/java/com/example/mrtan/weather/data/model/Weather.java
@@ -1,5 +1,6 @@
 package com.example.mrtan.weather.data.model;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,9 +8,49 @@ import java.util.List;
  */
 
 public final class Weather {
-    private Alarm mAlarm;//警告
+    private List<Alarm> mAlarms;//警告
     private Now mNow;//当前
     private List<Segment> mSegments;//时段
     private List<Quality> mQualities;//建议
     private List<Daily> mDailies;//每天
+
+    public List<Alarm> getAlarm() {
+        return mAlarms;
+    }
+
+    public void setAlarm(List<Alarm> alarms) {
+        mAlarms = alarms;
+    }
+
+    public Now getNow() {
+        return mNow;
+    }
+
+    public void setNow(Now now) {
+        mNow = now;
+    }
+
+    public List<Segment> getSegments() {
+        return mSegments;
+    }
+
+    public void setSegments(List<Segment> segments) {
+        mSegments = segments;
+    }
+
+    public List<Quality> getQualities() {
+        return mQualities;
+    }
+
+    public void setQualities(List<Quality> qualities) {
+        mQualities = qualities;
+    }
+
+    public List<Daily> getDailies() {
+        return mDailies;
+    }
+
+    public void setDailies(List<Daily> dailies) {
+        mDailies = dailies;
+    }
 }
